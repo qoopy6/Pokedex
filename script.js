27989863cb1aa8257053ac4eaf82b3c40b960a0b
qoopy6 <qoopy64@i.softbank.jp>
2026-01-22 12:52:37 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
 
 
-debugLog("script.js loaded");
-
-function debugLog(msg) {
-  const box = document.getElementById("debug");
-  if (!box) return;
-  box.innerHTML += msg + "<br>";
-}
-
 /* ========= 状態 ========= */
 
 const checkedSet = new Set();
